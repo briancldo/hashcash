09354045c972b4a86bb377f96c0c83df38eb576c
Brian Do <briancldo@gmail.com>
2021-04-09 18:53:10 -0400

    adds desired token to public info

--- public.py
+++ public.py
@@ -1,10 +1,7 @@
 hash_function = hash
 service = 'opm'
 work = 5
+desired_token = '1' * work
 
 def get_public_challenge():
-    return {
-        'hash_function': hash_function,
-        'service': service,
-        'work': work
-    }
+    return hash_function, service, work, desired_token
